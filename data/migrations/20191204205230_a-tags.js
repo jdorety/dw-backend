fbd1323956484b00e1ac0e6ce48dd4edff03198a
Jonathan Dorety <kyullog@gmail.com>
2019-12-04 21:08:54 -0800

    Create a-tag schema

--- data/migrations/20191204205230_a-tags.js
+++ data/migrations/20191204205230_a-tags.js
@@ -1,8 +1,10 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("a_tags", tbl => {
+    tbl.increments();
+    tbl.string("tag_name");
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("a_tags");
 };
