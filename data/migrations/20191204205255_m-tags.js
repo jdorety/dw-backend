1fb88c1f45c133270f1a4e2c46648ea5faa39433
Jonathan Dorety <kyullog@gmail.com>
2019-12-04 21:17:22 -0800

    Create monster tags schema

--- data/migrations/20191204205255_m-tags.js
+++ data/migrations/20191204205255_m-tags.js
@@ -1,8 +1,10 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("monster_tags", tbl => {
+    tbl.increments();
+    tbl.string("tag_name");
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("monster_tags");
 };
