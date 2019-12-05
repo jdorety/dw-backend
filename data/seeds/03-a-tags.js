0d41a653b657dc2ca5afa2588b01cf44385353d1
Jonathan Dorety <kyullog@gmail.com>
2019-12-05 12:13:28 -0800

    Complete attack tag seeds

--- data/seeds/03-a-tags.js
+++ data/seeds/03-a-tags.js
@@ -1,13 +1,17 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('a_tags').del()
-    .then(function () {
+  return knex("a_tags")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('a_tags').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("a_tags").insert([
+        { id: 1, tag_name: "Close" },
+        { id: 2, tag_name: "Reach" },
+        { id: 3, tag_name: "Messy" },
+        { id: 4, tag_name: "Near" },
+        { id: 5, tag_name: "Far" },
+        { id: 6, tag_name: "Forceful" },
+        { id: 7, tag_name: "Hand" }
       ]);
     });
 };
