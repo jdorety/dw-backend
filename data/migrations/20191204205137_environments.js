43997e2d7a0ff7a3c81618e2aa5fafaf7b32a179
Jonathan Dorety <kyullog@gmail.com>
2019-12-04 20:57:45 -0800

    Create environments schema

--- data/migrations/20191204205137_environments.js
+++ data/migrations/20191204205137_environments.js
@@ -1,8 +1,10 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("environments", tbl => {
+    tbl.increments();
+    tbl.string("name");
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("environments");
 };
